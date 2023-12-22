ef92640f6849922006436a9c04496addee7d9043
sc2bat <izar_dero@naver.com>
2023-12-22 15:36:10 +0900

    done independent card

--- lib/23_12_22_starbucks/main.dart
+++ lib/23_12_22_starbucks/main.dart
@@ -44,27 +44,42 @@ class MyStarBucksApp extends StatelessWidget {
                 description: '최근 주문',
                 descriptionColor: Colors.black),
             const QuickOrderCardListWidget(),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.teal[100 * (index % 9)],
-                  child: Text('etate'),
-                );
-              }, childCount: 20),
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0,
-              ),
-            ),
+            cardOnlyBoxWidget(imageUrl: '03_01_chrismas_event.png'),
+            cardOnlyBoxWidget(imageUrl: '05_01_card.png'),
+            cardOnlyBoxWidget(imageUrl: '05_02_card.png'),
             const BannerTitleWidget(
                 title: 'What\'s New',
                 titleColor: Colors.black,
                 description: 'See all',
                 descriptionColor: Colors.lightGreen),
             const NewsCardListWidget(),
+            cardOnlyBoxWidget(imageUrl: '06_01_card.png'),
+            cardOnlyBoxWidget(imageUrl: '06_02_card.png'),
+            cardOnlyBoxWidget(imageUrl: '06_03_card.png'),
+            cardOnlyBoxWidget(imageUrl: '06_04_card.png'),
+            cardOnlyBoxWidget(imageUrl: '06_05_card.png'),
           ],
         ),
         bottomNavigationBar: const BottomNavigationBarWidget(),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter cardOnlyBoxWidget({required String imageUrl}) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.6),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(2, 2),
+            )
+          ]),
+          child: Image.asset('assets/images/starbucks/$imageUrl'),
+        ),
       ),
     );
   }
