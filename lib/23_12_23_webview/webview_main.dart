79eb541deff1bf308bb2b23a350a9876a8d50a15
sc2bat <izar_dero@naver.com>
2023-12-23 20:44:53 +0900

    webview_flutter dependency 3.0+ to 4.0.0+

--- lib/23_12_23_webview/webview_main.dart
+++ lib/23_12_23_webview/webview_main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'widget/my_web_view_widget.dart';
 
 void main() {
   runApp(const MyWebViewApp());
 }
 
-class MyWebViewApp extends StatelessWidget {
+class MyWebViewApp extends StatefulWidget {
   const MyWebViewApp({super.key});
 
   @override
+  State<MyWebViewApp> createState() => _MyWebViewAppState();
+}
+
+class _MyWebViewAppState extends State<MyWebViewApp> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const MaterialApp(
+      title: 'my webview app',
+      home: MyWebViewWidget(),
+    );
   }
 }
---
+++ lib/23_12_23_webview/widget/my_web_view_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyWebViewWidget extends StatefulWidget {
+  const MyWebViewWidget({super.key});
+
+  @override
+  State<MyWebViewWidget> createState() => _MyWebViewWidgetState();
+}
+
+class _MyWebViewWidgetState extends State<MyWebViewWidget> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my webview app'),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
