da5dbc923422db9b0c2de5449a5293e3604eedd3
sc2bat <izar_dero@naver.com>
2023-12-24 13:04:39 +0900

    draw circle && add package sensors_plus

--- lib/23_12_24_horizontal_app/screen/my_horizontal_main_page_screen.dart
+++ lib/23_12_24_horizontal_app/screen/my_horizontal_main_page_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_logger/simple_logger.dart';
+
+final logger = SimpleLogger();
 
 class MyHorizontalMainPageScreen extends StatefulWidget {
   const MyHorizontalMainPageScreen({super.key});
@@ -12,13 +15,33 @@ class _MyHorizontalMainPageScreenState
     extends State<MyHorizontalMainPageScreen> {
   @override
   Widget build(BuildContext context) {
+    const containerWidth = 50.0;
+    const containerHeight = 50.0;
+
+    final centerX = MediaQuery.of(context).size.width / 2 - containerWidth / 2;
+    final centerY =
+        MediaQuery.of(context).size.height / 2 - containerHeight / 2;
+
+    logger.info('qwerasdf centerX, $centerX');
+    logger.info('qwerasdf centerY, $centerY');
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'my horizontal app',
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.red,
+              ),
+              width: containerWidth,
+              height: containerHeight,
+            ),
+          ),
+        ],
       ),
-      body: Column(),
     );
   }
 }
