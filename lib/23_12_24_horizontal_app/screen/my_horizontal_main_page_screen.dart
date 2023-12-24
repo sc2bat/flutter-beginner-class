4208e46ffa7b7246a67cb0a4b27553dbf05cfad3
sc2bat <izar_dero@naver.com>
2023-12-24 13:18:02 +0900

    done my horizontal app

--- lib/23_12_24_horizontal_app/screen/my_horizontal_main_page_screen.dart
+++ lib/23_12_24_horizontal_app/screen/my_horizontal_main_page_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:simple_logger/simple_logger.dart';
 
 final logger = SimpleLogger();
@@ -15,6 +17,11 @@ class _MyHorizontalMainPageScreenState
     extends State<MyHorizontalMainPageScreen> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
     const containerWidth = 50.0;
     const containerHeight = 50.0;
 
@@ -22,24 +29,44 @@ class _MyHorizontalMainPageScreenState
     final centerY =
         MediaQuery.of(context).size.height / 2 - containerHeight / 2;
 
-    logger.info('qwerasdf centerX, $centerX');
-    logger.info('qwerasdf centerY, $centerY');
+    // logger.info('qwerasdf centerX, $centerX');
+    // logger.info('qwerasdf centerY, $centerY');
 
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.red,
-              ),
-              width: containerWidth,
-              height: containerHeight,
-            ),
-          ),
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEventStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                // logger.info('qwerasdf $snapshot');
+
+                final event = snapshot.data!;
+                List<double> accelerometerValues = [
+                  event.x,
+                  event.y,
+                  event.z,
+                ];
+                logger
+                    .info('qwerasdf accelerometerValues $accelerometerValues');
+
+                return Positioned(
+                  left: centerX + accelerometerValues[1] * 15,
+                  top: centerY + accelerometerValues[0] * 15,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                    width: containerWidth,
+                    height: containerHeight,
+                  ),
+                );
+              }),
         ],
       ),
     );
