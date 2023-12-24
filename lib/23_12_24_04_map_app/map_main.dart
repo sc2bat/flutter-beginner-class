82777dcac857ca2902b6561fca3a968908f8ec27
sc2bat <izar_dero@naver.com>
2023-12-24 16:17:52 +0900

    start my map app

---
+++ lib/23_12_24_04_map_app/map_main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screen/my_map_home_page_screen.dart';
+
+void main() {
+  runApp(MyMapApp());
+}
+
+class MyMapApp extends StatelessWidget {
+  const MyMapApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'my map app',
+      home: MyMapHomePageScreen(),
+    );
+  }
+}
