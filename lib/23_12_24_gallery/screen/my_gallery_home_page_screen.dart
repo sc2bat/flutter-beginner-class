faf5209dbf84cec01e37e05817ab7a4b2d996d7a
sc2bat <izar_dero@naver.com>
2023-12-24 10:44:06 +0900

    image picker flutter infinite loading

--- lib/23_12_24_gallery/screen/my_gallery_home_page_screen.dart
+++ lib/23_12_24_gallery/screen/my_gallery_home_page_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:simple_logger/simple_logger.dart';
+
+final logger = SimpleLogger();
 
 class MyGalleryHomePageScreen extends StatefulWidget {
   const MyGalleryHomePageScreen({super.key});
@@ -9,13 +15,56 @@ class MyGalleryHomePageScreen extends StatefulWidget {
 }
 
 class _MyGalleryHomePageScreenState extends State<MyGalleryHomePageScreen> {
+  final ImagePicker _imagePicker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('my gallery app'),
       ),
-      body: Image.network('https://place-hold.it/300x500/666/fff/000.gif'),
+      body:
+          // Image.network('https://place-hold.it/300x500/666/fff/000.gif'),
+          images == null
+              ? const Center(
+                  child: Text(
+                    'My Gallery',
+                  ),
+                )
+              : FutureBuilder<Uint8List>(
+                  future: images![0].readAsBytes(),
+                  builder: (context, snapshot) {
+                    final data = snapshot.data;
+
+                    if (data == null ||
+                        snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    return Image.memory(
+                      data,
+                      width: double.infinity,
+                    );
+                  },
+                ),
     );
+  }
+
+  Future<void> loadImages() async {
+    images = await _imagePicker.pickMultiImage();
+    setState(() {
+      logger.info('qwerasdf loadImages Function setState');
+    });
+
+    logger.info('qwerasdf $images');
   }
 }
