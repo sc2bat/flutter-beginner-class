596289b0d1c6f5f062c16a8f07989c063166cdeb
sc2bat <izar_dero@naver.com>
2023-12-24 11:13:38 +0900

    done gallery ap

--- lib/23_12_24_gallery/screen/my_gallery_home_page_screen.dart
+++ lib/23_12_24_gallery/screen/my_gallery_home_page_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -17,11 +18,12 @@ class MyGalleryHomePageScreen extends StatefulWidget {
 class _MyGalleryHomePageScreenState extends State<MyGalleryHomePageScreen> {
   final ImagePicker _imagePicker = ImagePicker();
   List<XFile>? images;
+  int _currentPage = 0;
+  final _pageController = PageController();
 
   @override
   void initState() {
     super.initState();
-
     loadImages();
   }
 
@@ -29,7 +31,7 @@ class _MyGalleryHomePageScreenState extends State<MyGalleryHomePageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my gallery app'),
+        title: const Text('my gallery app'),
       ),
       body:
           // Image.network('https://place-hold.it/300x500/666/fff/000.gif'),
@@ -39,28 +41,49 @@ class _MyGalleryHomePageScreenState extends State<MyGalleryHomePageScreen> {
                     'My Gallery',
                   ),
                 )
-              : FutureBuilder<Uint8List>(
-                  future: images![0].readAsBytes(),
-                  builder: (context, snapshot) {
-                    final data = snapshot.data;
+              : PageView(
+                  controller: _pageController,
+                  children: images!.map(
+                    (e) {
+                      return FutureBuilder<Uint8List>(
+                        future: e.readAsBytes(),
+                        builder: (context, snapshot) {
+                          final data = snapshot.data;
 
-                    if (data == null ||
-                        snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
+                          if (data == null ||
+                              snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          return Image.memory(
+                            data,
+                            width: double.infinity,
+                          );
+                        },
                       );
-                    }
-                    return Image.memory(
-                      data,
-                      width: double.infinity,
-                    );
-                  },
+                    },
+                  ).toList(),
                 ),
     );
   }
 
   Future<void> loadImages() async {
     images = await _imagePicker.pickMultiImage();
+
+    if (images != null) {
+      Timer.periodic(const Duration(seconds: 3), (timer) {
+        logger.info('qwerasdf $_currentPage');
+        _currentPage++;
+        if (_currentPage > images!.length - 1) {
+          _currentPage = 0;
+        }
+        _pageController.animateToPage(_currentPage,
+            duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
+      });
+    }
+
     setState(() {
       logger.info('qwerasdf loadImages Function setState');
     });
